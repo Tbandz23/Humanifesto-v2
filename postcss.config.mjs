393f3381cc95349f58c443f3bb097a07d4d761f2
Tomas Cakulev <tomas.cakulev@icloud.com>
2025-07-23 19:29:27 +0200

    feat: Add settings page and header post button

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,13 @@
+<<<<<<< HEAD
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
     tailwindcss: {},
   },
+=======
+const config = {
+  plugins: ["@tailwindcss/postcss"],
+>>>>>>> cd4a361 (Initial commit from Create Next App)
 };
 
 export default config;
